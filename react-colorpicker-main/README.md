3089ff76a70328be0ceee80f55feca4c1d64977b
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2023-11-10 18:07:04 +0530

    Add files via upload

---
+++ react-colorpicker-main/README.md
@@ -0,0 +1,3 @@
+![Task31](task31.png)
+![Task31](task32.png)
+https://react-colorpicker-delta.vercel.app/
---
+++ react-colorpicker-main/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+
+const ColorPicker = ({ colors }) => {
+  const [selectedColor, setSelectedColor] = React.useState(null);
+  const [displayColorList, setDisplayColorList] = React.useState(false);
+
+  const handleButtonClick = () => {
+    setDisplayColorList(!displayColorList);
+  };
+
+  const handleColorClick = (color) => {
+    setSelectedColor(color);
+    setDisplayColorList(false);
+  };
+
+  const colorSquares = colors.map((color, index) => (
+    <div
+      key={index}
+      className="color-square"
+      style={{ backgroundColor: color }}
+      onClick={() => handleColorClick(color)}
+    ></div>
+  ));
+
+  const colorList = displayColorList ? (
+    <div className="color-list">
+      {colorSquares}
+    </div>
+  ) : null;
+
+  return (
+    <div className="app">
+      <div className="color-picker" style={{ backgroundColor: selectedColor || 'purple' }}>
+        {colorList}
+        <button onClick={handleButtonClick} style={{ backgroundColor: 'green' }}>
+          Pick a color
+        </button>
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  const colors = ['red', 'blue', 'yellow', 'green', 'orange', 'pink', 'purple', 'brown', 'gray', 'cyan', 'magenta', 'violet', 'indigo', 'teal', 'lime', 'olive', 'maroon', 'navy', 'fuchsia', 'aqua'];
+
+  return <ColorPicker colors={colors} />;
+}
+
+export default App;
+
